fdad6b304c5c0548628786b7aec3f417ccf24b6f
Billy Wilson Arante <arantebillywilson@gmail.com>
2018-02-11 11:13:06 +0800

    Feat: New file assignment_operator.c

---
+++ assignment_operator.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+        //int score;
+        int score1;
+        int score2;
+        int sum;
+
+        //score = 10;
+        score1 = 32;
+        //printf("Score 1: %d\n", score1);
+
+        //score = 20;
+        score2 = 23;
+        //printf("Score 2: %d\n", score2);
+
+        sum = score1 + score2;
+        //printf("Sum: %d\n", sum);
+
+        printf("Score 1: %d, Score 2: %d, Sum: %d\n", score1, score2, sum);
+
+        return 0;
+}
