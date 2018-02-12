1ba1113ac37683f6e9b1bddee71244f2129b2992
Billy Wilson Arante <arantebillywilson@gmail.com>
2018-02-12 08:16:12 +0800

    Feat: New file average.c

---
+++ average.c
@@ -0,0 +1,23 @@
+/**
+ * average.c
+ *
+ * Written by Billy Wilson Arante <arantebillywilson@gmail.com>
+ * Last updated on 2018/02/12 PHT
+ */
+
+#include <stdio.h>
+
+int main()
+{
+        int firstNumber = 1;
+        int secondNumber = 2;
+
+        //int average;
+        float average;
+
+        average = (firstNumber + secondNumber) / 2.0;
+
+        printf("Average: %f\n", average);
+
+        return 0;
+}
