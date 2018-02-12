bf6490a87305d6dfcbe6f1909eaa5c5ca86ac91e
Billy Wilson Arante <arantebillywilson@gmail.com>
2018-02-12 08:16:31 +0800

    Feat: New file character.c

---
+++ character.c
@@ -0,0 +1,21 @@
+/**
+ * character.c
+ *
+ * Written by Billy Wilson Arante <arantebillywilson@gmail.com>
+ * Last updated on 2018/02/12 PHT
+ */
+
+#include <stdio.h>
+
+int main()
+{
+        char letter;  // ASCII characters
+
+        letter = 'a';
+        printf("Character 'a': %c\n", letter);
+
+        letter = 97;
+        printf("Character 97: %c\n", letter);
+
+        return 0;
+}
