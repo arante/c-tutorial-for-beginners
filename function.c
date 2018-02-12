1ad23f25ea93c90a44577817b770160e3fda6576
Billy Wilson Arante <arantebillywilson@gmail.com>
2018-02-12 08:35:35 +0800

    Feat: New file function.c

---
+++ function.c
@@ -0,0 +1,26 @@
+/**
+ * function.c
+ *
+ * Written by Billy Wilson Arante <arantebillywison@gmail.com>
+ * Last updated on 2018/02/12 PHT
+ */
+
+#include <stdio.h>
+
+void helloWorld()
+{
+        printf("Hello world! I am ready for C.\n");
+}
+
+int main()
+{
+        //printf("Hello world! I am ready for C.\n");
+        //printf("Hello world! I am ready for C.\n");
+        //printf("Hello world! I am ready for C.\n");
+
+        helloWorld();
+        helloWorld();
+        helloWorld();
+
+        return 0;
+}
