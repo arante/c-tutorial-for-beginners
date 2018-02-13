b8ad0967f0bbb871010b109acaf045b1b07919d1
Billy Wilson Arante <arantebillywilson@gmail.com>
2018-02-14 00:07:53 +0800

    Update: Completed cricket_switch.c program

--- cricket_switch.c
+++ cricket_switch.c
@@ -1,6 +1,36 @@
-#include <stdio.c>
+/**
+ * cricket_switch.c
+ *
+ * Written by Billy Wilson Arante <arantebillywilson@gmail.com>
+ * Last updated on 2018/05/14 PHT
+ */
+
+#include <stdio.h>
 
 int main()
 {
+        int run = 3;
+
+        switch (run) {
+                case 1:
+                        printf("Single\n");
+                        break;
+                case 2:
+                        printf("Double\n");
+                        break;
+                case 3:
+                        printf("Triple\n");
+                        break;
+                case 4:
+                        printf("Boundary\n");
+                        break;
+                case 6:
+                        printf("Sixer\n");
+                        break;
+                default:
+                        printf("Penalty Run\n");
+                        break;
+        }
+
         return 0;
 }
